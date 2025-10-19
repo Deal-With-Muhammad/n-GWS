725db6f8423b2603572939a1978c93f7962468a1
imabotbro <ilbigboss21@gmail.com>
2025-10-19 23:18:45 +0800

    feat: fix build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,13 @@ import { useState } from "react";
 import { LoaderWindow } from "../components/loaderWindow/index";
 import { SectionHero } from "../components/Main/SectionHero";
 export default function Home() {
-  const [displayed, setDisplayed] = useState(0);
   const [loaderFinished, setLoaderFinished] = useState(false);
 
   return (
     <>
       <SectionHero />
 
-      <LoaderWindow
-        setLoaderFinished={setLoaderFinished}
-        displayed={displayed}
-        setDisplayed={setDisplayed}
-      />
+      <LoaderWindow setLoaderFinished={setLoaderFinished} />
     </>
   );
 }
