5603923a80f1c73f9b8d060b076975a318e3549f
imabotbro <ilbigboss21@gmail.com>
2025-10-23 03:41:10 +0800

    feat: add ts ignore build errors

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // ✅ Ignore TypeScript build errors
+    ignoreBuildErrors: true,
+  },
+};
 
 module.exports = nextConfig;
